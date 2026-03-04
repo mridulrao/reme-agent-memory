f9f20238fbb9ca71816a72d88680607d81635ea5
mridulrao <mridulrao674385@gmail.com>
2026-03-03 16:42:46 -0800

    Code Refactor

--- tests/test_logo.py
+++
@@ -1,9 +0,0 @@
-"""test logo"""
-
-from reme.core.schema import ServiceConfig, MCPConfig
-
-if __name__ == "__main__":
-    from reme.core.utils import print_logo
-
-    c = ServiceConfig(app_name="reme", backend="mcp", mcp=MCPConfig(transport="sse"))
-    print_logo(service_config=c)
